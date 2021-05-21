695873a1b7ae3d6ac296cfe9f13d8c74ebe9ce22
Ajat Darojat <ajatdarojat45@gmail.com>
2021-05-21 16:35:11 +0700

    add buy me a coffee button

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,14 @@
 export default function Footer() {
   return (
     <>
-      <footer className="text-center m-10">
+      <footer className="m-10 flex justify-center flex-col items-center">
+        <a href="https://www.buymeacoffee.com/ajatdarojat45" target="_blank">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            style={{ height: 60, width: 217, marginBottom: 10 }}
+          />
+        </a>
         <p>Made with ❤️</p>
       </footer>
     </>
--- pages/[slug].js
+++ pages/[slug].js
@@ -93,6 +93,18 @@ function Detail({ data, image }) {
         `,
           }}
         />
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="ajatdarojat45"
+          data-description="Support me on Buy me a coffee!"
+          data-message=""
+          data-color="#FFDD00"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        ></script>
       </Head>
 
       <main className="container mx-auto">
